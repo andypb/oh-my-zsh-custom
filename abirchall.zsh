18f0baae0a57239dbde7a4ec342434f16675857a
andypb <andy.birchall@gmail.com>
2022-12-02 14:24:41 +0000

    alias for ls to get colours for gls

--- abirchall.zsh
+++ abirchall.zsh
@@ -3,6 +3,7 @@ alias gloga="git log -30 --all --graph --pretty='format:%C(yellow)%h %C(cyan)%d 
 alias glogab="git log -30 --graph --pretty='format:%C(yellow)%h %C(cyan)%d %Cgreen%ad %C(bold blue)%an: %Creset%Cred%<(70,trunc)%s%Creset' --date=short"
 alias glogao="git log -30 --pretty='format:%C(yellow)%h %C(bold blue)%d %C(green)%ad %C(cyan)%an %Creset%s' --date=short"
 alias glap='git pull --all --prune'
+alias ls='ls --color'
 alias lla='ls -lha'
 alias emcnw='emacs -nw'
 alias cdp='cd -P'
