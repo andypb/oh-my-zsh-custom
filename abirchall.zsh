46be129419ff4f00b7e574d9dc3c9a1c4b428d3a
andypb <andy.birchall@gmail.com>
2023-03-24 11:40:32 +0000

    new aliases and backup .zshrc

--- abirchall.zsh
+++ abirchall.zsh
@@ -3,9 +3,10 @@ alias gloga="git log -50 --all --graph --pretty='format:%C(yellow)%h %C(cyan)%d 
 alias glogab="git log -50 --graph --pretty='format:%C(yellow)%h %C(cyan)%d %Cgreen%ad %C(bold blue)%an: %Creset%Cred%<(70,trunc)%s%Creset' --date=short"
 alias glogao="git log -50 --pretty='format:%C(yellow)%h %C(bold blue)%d %C(green)%ad %C(cyan)%an %Creset%s' --date=short"
 alias glap='git pull --all --prune'
+alias gla='git pull --all'
 alias ls='ls --color'
 alias lla='ls -lha'
-alias emcnw='emacs -nw'
+alias emacsnw='emacs -nw'
 alias cdp='cd -P'
 
 ## pager
